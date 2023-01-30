01addcc3a0efeddad964c3355712b5e9ef66d982
Guilherme Galindo <guilhermegalindo@discente.ufg.br>
2023-01-30 11:28:33 -0300

    Iterando os comentários

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,47 @@ import { Post } from './components/Post';
 import style from './App.module.css';
 import './global.css';
 
+/*
+
+author: {avatar_url: "", name: "", role: ""}
+publishedAt: Date
+content: String
+
+*/
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [{ type: 'paragraph', content: 'Fala galera!!' },
+    { type: 'paragraph', content: 'Acabei de subir mais um portifólio. É um projeto que fiz no NLW Return, evento da RocketSeat.' },
+    { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-01-27 20:00:00'),
+
+
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat"
+    },
+    content: [{ type: 'paragraph', content: 'Fala galera!!' },
+    { type: 'paragraph', content: 'Acabei de subir mais um portifólio. É um projeto que fiz no NLW Return, evento da RocketSeat.' },
+    { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-01-29 20:00:00'),
+
+
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -12,11 +53,9 @@ export function App() {
       <div className={style.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return <Post author={post.author} content={post.content} publishedAt={post.publishedAt} />
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,43 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="http://github.com/diego3g.png" alt="" />
+                    <Avatar src={author.avatarUrl} alt="" />
                     <div className={styles.authorInfo}>
-                        <strong>Diego Fernandes</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:35">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content} >
-                <p>Fala galera!!</p>
-                <p>Acabei de subir mais um portifólio. É um projeto que fiz no NLW Return, evento da RocketSeat.</p>
-                <p><a href='#'>jane.design/doctorcare</a> </p>
-                <p><a href='#'>#novoprojeto</a> <a href='#'>#nlw</a> <a href='#'>#rocketseat</a></p>
+                {content.map(line => {
+                    if (line.type == 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link') {
+                        return <p><a href='#'>{line.content}</a></p>
+                    }
+                })}
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu comentário!</strong>
